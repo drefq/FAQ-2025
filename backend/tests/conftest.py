f8258b8a14fbdb54040312cfd36e8f96a9bd2984
Igor Baliuk <baliuk.igor@gmail.com>
2025-01-18 21:35:48 +0300

    [backend] Rename stat field and fix typos

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,7 +3,7 @@ from collections.abc import MutableMapping
 from typing import Any
 
 
-def pytest_tavern_beta_before_every_request(request_args: MutableMapping) :
+def pytest_tavern_beta_before_every_request(request_args: MutableMapping):
     logging.info(f"Request: {request_args['method']} {request_args['url']}\n{request_args['headers']}\n{request_args.get('body', "<no body>")}")
 
 def pytest_tavern_beta_after_every_response(expected: Any, response: Any) -> None:
